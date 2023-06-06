ab4e7901d81255d761dd27c10d1186802ba7f128
Nam Nguyen <nam.ngh2211@mail.com>
2023-06-06 19:09:24 +0700

    First commit

---
+++ Assets/DragAndDrop.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    [SerializeField] GameObject particleVFX;
+
+    private bool _dragging;
+
+    private Vector2 _offset;
+
+    public static bool mouseButtonReleased;
+
+    private void OnMouseDown()
+    {
+        _dragging = true;
+
+        _offset = GetMousePos() - (Vector2)transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!_dragging) return;
+
+        var mousePosition = GetMousePos();
+
+        transform.position = mousePosition - _offset;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _dragging = false;
+        if (gameObject.tag == collision.gameObject.tag)
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            GameObject explosion = Instantiate(particleVFX, transform.position, transform.rotation);
+            Destroy(explosion, .75f);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        mouseButtonReleased = true;
+    }
+
+    private Vector2 GetMousePos()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+}
